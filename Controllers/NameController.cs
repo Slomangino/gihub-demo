13d681264fad2c5e82130e3bcf71569f89544c17
Stephen Lomangino <iceman371@yahoo.com>
2018-10-27 15:54:29 -0400

    Added name controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace github_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public Person Get()
+        {
+            var me = new Person()
+            {
+                Name = "Stephen",
+                Age = 26,
+            };
+
+            return me;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
