c74abc94a3e430ca943dc6bc431f423a051420c2
Stephen Lomangino <iceman371@yahoo.com>
2018-10-27 16:50:22 -0400

    Changed back to my name

--- Controllers/NameController.cs
+++ Controllers/NameController.cs
@@ -4,22 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace github_demo.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class NameController : Controller
     {
-        // GET: api/<controller>
         [HttpGet]
         public Person Get()
         {
+            // create local object
             var me = new Person()
             {
-                Name = "Tyler",
-                Age = 29,
+                Name = "Stephen",
+                Age = 26,
             };
 
             return me;
