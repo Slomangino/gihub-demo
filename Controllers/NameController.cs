4a9b3d82274d15aa2f9d5ef4a80d081c2e0d3196
T. Hall <>
2018-10-29 09:52:47 -0400

    - Added CreatePerson POST method

--- Controllers/NameController.cs
+++ Controllers/NameController.cs
@@ -11,22 +11,50 @@ namespace github_demo.Controllers
     public class NameController : Controller
     {
         [HttpGet]
-        public Person Get()
+        public Person GetWithId(int id)
         {
-            // create local object
-            var me = new Person()
-            {
-                Name = "Stephen",
-                Age = 26,
-            };
+            var person = People.FirstOrDefault(a => a.Id == id);
 
-            return me;
+            return person;
         }
+
+        [HttpPost]
+        public int CreatePerson([FromBody] PersonInputDTO input)
+        {
+            var random = new Random();
+
+            People.Add(new Person
+            {
+                Id = random.Next(2, 200),
+                Name = input.Name,
+                Age = input.Age ?? 25
+            });
+
+            return People.Last().Id;
+        }
+
+        public List<Person> People = new List<Person>(new List<Person>
+        { 
+            new Person()
+            {
+                    Id = 1,
+                    Name = "Stephen",
+                    Age = 26
+            }
+        });
     }
 
     public class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+    }
+
+    public class PersonInputDTO
+    {
+        public string Name { get; set; } 
+
+        public int? Age { get; set; }
     }
 }
